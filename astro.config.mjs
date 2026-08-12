db3eca3675a99a98fa815b9a74903997bca59d5c
Josué Mayoral <josue.mayoral@alumnos.udg.mx>
2026-08-11 22:56:21 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,10 @@ import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://josuemayoral-cell.github.io',
-  base: '/josuemayoral.github.io',
   vite: {
     plugins: [tailwindcss()]
   },
   integrations: [mdx(), sitemap()]
 });
-
